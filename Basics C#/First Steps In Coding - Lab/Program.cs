62d339a7d524d35311a9c06700d384825509c02b
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-03 22:07:54 +0300

    Add files via upload

---
+++ Basics C#/First Steps In Coding - Lab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InchesToCentimetres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inches = double.Parse(Console.ReadLine());
+            double centimetres = inches * 2.54;
+            Console.WriteLine(centimetres);
+        }
+    }
+}
