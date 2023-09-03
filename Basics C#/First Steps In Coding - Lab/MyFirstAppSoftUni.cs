1d9df0a36fee4385361849ec2522d1642a05aea9
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-03 22:09:59 +0300

    Create MyFirstAppSoftUni.cs

---
+++ Basics C#/First Steps In Coding - Lab/MyFirstAppSoftUni.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyFirstAppSoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {firstName}!");
+        }
+    }
+}
