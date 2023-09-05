fcc3a0e128225fa64dd6eccc6aabfd1f7af0430f
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-05 16:19:24 +0300

    Create 04. Sequence 2k+1.cs

---
+++ Basics C#/While Loop - Lab/04. Sequence 2k+1.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace _04._Sequence_2k_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = 1;
+            while (k <= n)
+            {
+                Console.WriteLine(k);
+                k = k * 2 + 1;
+            } 
+        }
+    }
+}
