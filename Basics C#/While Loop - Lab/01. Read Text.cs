23d95cc92f1ef9724e84510d342d95a0b5aeb07c
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-05 15:42:07 +0300

    Create 01. Read Text.cs

---
+++ Basics C#/While Loop - Lab/01. Read Text.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace _01._Read_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "Stop")
+            {
+                Console.WriteLine(input);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
