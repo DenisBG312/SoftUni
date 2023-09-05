ddcb93e573b9fd9e22708c58509680535b02ca5f
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-05 11:47:27 +0300

    Create 08. Number sequence.cs

---
+++ Basics C#/For Loop - Lab/08. Number sequence.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace _08._Number_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num > maxNum)
+                {
+                    maxNum = num;
+                }
+                if (minNum > num)
+                {
+                    minNum = num;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNum}");
+            Console.WriteLine($"Min number: {minNum}");
+        }
+    }
+}
