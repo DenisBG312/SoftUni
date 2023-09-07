5e74d0f0586e44db0a43f079c3255a2958fc6836
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-07 13:32:54 +0300

    Create 05. Travelling.cs

---
+++ Basics C#/Nested Loops - Lab/05. Travelling.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace _05._Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                double budget = double.Parse(Console.ReadLine());
+                double money = 0;
+                while (money < budget)
+                {
+                    double moreMoney = double.Parse(Console.ReadLine());
+
+                    money += moreMoney;
+                }
+                Console.WriteLine($"Going to {input}!");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
