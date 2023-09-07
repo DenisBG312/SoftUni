af5d09429dbbb26faddda32bbda8da33228d86cd
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-07 10:24:09 +0300

    Create 04. Walking.cs

---
+++ Basics C#/While Loop - Exercise/04. Walking.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace _04._Walking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int steps = 0;
+            string input;
+            while (steps < 10000)
+            {
+                input = Console.ReadLine();
+                if (input == "Going home")
+                {
+                    steps += int.Parse(Console.ReadLine());
+                    break;
+                }
+                steps += int.Parse(input);
+            }
+            if (steps >= 10000)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{steps - 10000} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{10000 - steps} more steps to reach goal.");
+            }
+        }
+    }
+}
