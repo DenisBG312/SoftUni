984e36424e4163ead8512ebed883900cabf4bcf5
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-12 23:55:33 +0300

    Create 02. Passed.cs

---
+++ Fundamentals C#/Basic Syntax, Conditional Statements and Loops - Lab/02. Passed.cs
@@ -0,0 +1,15 @@
+namespace _02._Passed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 3)
+            {
+                Console.WriteLine($"Passed!");
+            }
+        }
+    }
+}
