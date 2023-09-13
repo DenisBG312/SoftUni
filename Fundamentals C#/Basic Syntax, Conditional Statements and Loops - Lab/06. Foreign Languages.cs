ceb4e8f6c674d25ee4dc9b614ec9bc3fc09f6c98
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-13 22:37:31 +0300

    Create 06. Foreign Languages.cs

---
+++ Fundamentals C#/Basic Syntax, Conditional Statements and Loops - Lab/06. Foreign Languages.cs
@@ -0,0 +1,23 @@
+namespace _06._Foreign_Languages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string countryName = Console.ReadLine();
+
+            if (countryName == "USA" || countryName == "England")
+            {
+                Console.WriteLine("English");
+            }
+            else if (countryName == "Spain" || countryName == "Argentina" || countryName == "Mexico")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
