bff3a85aac206d36689a79588566b35d64b94dc4
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-17 20:32:41 +0300

    Create 02. English Name of the Last Digit.cs

---
+++ Fundamentals C#/Basic Syntax, Conditional Statements and Loops - More Exercise/02. English Name of the Last Digit.cs
@@ -0,0 +1,49 @@
+long num = long.Parse(Console.ReadLine());
+long lastD = FindLastDigits(num);
+
+long FindLastDigits(long num)
+{
+    long lastDigs = Math.Abs(num % 10);
+    return lastDigs;
+}
+
+if (num == 1)
+{
+    Console.WriteLine("one");
+}
+else if (lastD == 2)
+{
+    Console.WriteLine("two");
+}
+else if (lastD == 3)
+{
+    Console.WriteLine("three");
+}
+else if (lastD == 4)
+{
+    Console.WriteLine("four");
+}
+else if (lastD == 5)
+{
+    Console.WriteLine("five");
+}
+else if (lastD == 6)
+{
+    Console.WriteLine("six");
+}
+else if (lastD == 7)
+{
+    Console.WriteLine("seven");
+}
+else if (lastD == 8)
+{
+    Console.WriteLine("eight");
+}
+else if (lastD == 9)
+{
+    Console.WriteLine("nine");
+}
+else if (lastD == 0)
+{
+    Console.WriteLine("zero");
+} 
