294287c9571dce782b550679dab509b2b70132c8
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-22 02:07:26 +0300

    Update 06. Reversed Chars.cs

--- Fundamentals C#/Data Types and Variables - Lab/06. Reversed Chars.cs
+++ Fundamentals C#/Data Types and Variables - Lab/06. Reversed Chars.cs
@@ -1,15 +1,5 @@
-int endNum = int.Parse(Console.ReadLine());
+string first = Console.ReadLine();
+string second = Console.ReadLine();
+string third = Console.ReadLine();
 
-for (int i = 1; i <= endNum; i++)
-{
-    int number = i;
-    int sum = 0;
-    while (number > 0)
-    {
-        int lastDigit = number % 10;
-        sum += lastDigit;
-        number /= 10;
-    }
-    bool isValid = sum == 5 || sum == 7 || sum == 11;
-    Console.WriteLine($"{i} -> " + isValid);
-}
+Console.Write($"{third} {second} {first}");
