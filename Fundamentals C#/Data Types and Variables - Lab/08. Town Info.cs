ea0429112cddc472bcd3648a7b28475d7ca58bac
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-22 18:45:28 +0300

    Create 08. Town Info.cs

---
+++ Fundamentals C#/Data Types and Variables - Lab/08. Town Info.cs
@@ -0,0 +1,5 @@
+string townName = Console.ReadLine();
+int population = int.Parse(Console.ReadLine());
+int area = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
