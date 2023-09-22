ac5ebd59eaa649f41883099d9272edb4a885b7ed
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-22 18:47:28 +0300

    Create 09. Chars to String.cs

---
+++ Fundamentals C#/Data Types and Variables - Lab/09. Chars to String.cs
@@ -0,0 +1,5 @@
+char first = char.Parse(Console.ReadLine());
+char second = char.Parse(Console.ReadLine());
+char third = char.Parse(Console.ReadLine());
+
+Console.WriteLine($"{first}{second}{third}");
