314e659a45f37e3e593669b4561fad2d46acd6ca
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2023-09-23 17:42:48 +0300

    Create 05. Print Part Of ASCII Table.cs

---
+++ Fundamentals C#/Data Types and Variables - Exercise/05. Print Part Of ASCII Table.cs
@@ -0,0 +1,16 @@
+namespace _05._Print_Part_Of_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startIndex = int.Parse(Console.ReadLine());
+            int endIndex = int.Parse(Console.ReadLine());
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
