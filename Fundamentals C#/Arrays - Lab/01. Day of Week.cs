ae979673cb5e7dc9a610372d315e70e8d048bbe0
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2023-09-29 21:21:43 +0300

    Update 01. Day of Week.cs

--- Fundamentals C#/Arrays - Lab/01. Day of Week.cs
+++ Fundamentals C#/Arrays - Lab/01. Day of Week.cs
@@ -1,1 +1,30 @@
+namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] day =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
 
+            int dayOfWeek = int.Parse(Console.ReadLine());
+
+            if (dayOfWeek >= 1 && dayOfWeek <= 7)
+            {
+                Console.WriteLine(day[dayOfWeek - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
