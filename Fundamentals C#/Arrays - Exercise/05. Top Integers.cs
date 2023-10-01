27790f58b721cbe692825e48d6c6262faf5851e7
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2023-10-01 13:46:40 +0300

    Create 05. Top Integers.cs

---
+++ Fundamentals C#/Arrays - Exercise/05. Top Integers.cs
@@ -0,0 +1,29 @@
+namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isTopInteger = true;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] <= arr[j])
+                    {
+                        isTopInteger = false;
+                    }
+                }
+                if (isTopInteger)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+        }
+    }
+}
