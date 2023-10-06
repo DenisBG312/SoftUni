a5407285de53d4c572d383007407d773b5542215
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2023-10-06 20:09:27 +0300

    Create 08. Magic Sum.cs

---
+++ Fundamentals C#/Arrays - Exercise/08. Magic Sum.cs
@@ -0,0 +1,27 @@
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int toBeEqual = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int currentNum = arr[i];
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (currentNum + arr[j] == toBeEqual)
+                    {
+                        Console.WriteLine($"{currentNum} {arr[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
