384678a588133b259f9e2758e6eccc1a4e3622d7
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2023-10-06 22:05:04 +0300

    Update 01. Sign of Integer Numbers.cs

--- Fundamentals C#/Methods - Lab/01. Sign of Integer Numbers.cs
+++ Fundamentals C#/Methods - Lab/01. Sign of Integer Numbers.cs
@@ -1,1 +1,27 @@
+namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintNumber(number);
 
+            static void PrintNumber(int number)
+            {
+                if (number > 0)
+                {
+                    Console.WriteLine($"The number {number} is positive.");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine($"The number {number} is negative.");
+                }
+                else
+                {
+                    Console.WriteLine($"The number {number} is zero.");
+                }
+            }
+        }
+    }
+}
