283175cb8c5c0c942e3632475da089ddb176cc04
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2023-10-12 21:38:18 +0300

    Create 05. Remove Negatives and Reverse.cs

---
+++ Fundamentals C#/Lists - Lab/05. Remove Negatives and Reverse.cs
@@ -0,0 +1,34 @@
+namespace _05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+
+            numbers.Reverse();
+            Console.WriteLine(string.Join(" ", numbers));
+
+
+        }
+    }
+}
