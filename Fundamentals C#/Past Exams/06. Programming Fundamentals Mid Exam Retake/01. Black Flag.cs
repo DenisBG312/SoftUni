fe56ae0bfd9544eb124134e6f1af0f256ccbe18b
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2023-10-21 20:08:45 +0300

    Create 01. Black Flag.cs

---
+++ Fundamentals C#/Past Exams/06. Programming Fundamentals Mid Exam Retake/01. Black Flag.cs
@@ -0,0 +1,37 @@
+namespace _01._Black_Flag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysPlunder = int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double totalPlunder = 0;
+
+            for (int day = 1; day <= daysPlunder; day++)
+            {
+                totalPlunder += dailyPlunder;
+                if (day % 3 == 0)
+                {
+                    totalPlunder += dailyPlunder * 0.5; // TODO {dayliPlunder} should maybe be double?
+                }
+                if (day % 5 == 0)
+                {
+                    totalPlunder *= 0.7;
+                }
+            }
+
+            if (totalPlunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:f2} plunder gained.");
+            }
+            else
+            {
+                double percentageCollected = (totalPlunder / expectedPlunder) * 100;
+                Console.WriteLine($"Collected only {percentageCollected:f2}% of the plunder.");
+            }
+        }
+    }
+}
