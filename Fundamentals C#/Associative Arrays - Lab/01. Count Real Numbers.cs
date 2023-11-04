7e163105182d38db505bd3d2538da10608b76635
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2023-11-04 14:55:11 +0200

    Update 01. Count Real Numbers.cs

--- Fundamentals C#/Associative Arrays - Lab/01. Count Real Numbers.cs
+++ Fundamentals C#/Associative Arrays - Lab/01. Count Real Numbers.cs
@@ -1,1 +1,32 @@
+namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
 
+            SortedDictionary<int, int> numbersDictionary = new SortedDictionary<int, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbersDictionary.ContainsKey(numbers[i]))
+                {
+                    numbersDictionary[numbers[i]]++;
+                }
+                else
+                {
+                    numbersDictionary[numbers[i]] = 1;
+                }
+            }
+
+            foreach (KeyValuePair<int, int> number in numbersDictionary)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
