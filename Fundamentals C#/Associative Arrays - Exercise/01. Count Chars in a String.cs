5981b849cc9c8a6916eb37b80803662633e42521
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2023-11-05 13:38:54 +0200

    Update 01. Count Chars in a String.cs

--- Fundamentals C#/Associative Arrays - Exercise/01. Count Chars in a String.cs
+++ Fundamentals C#/Associative Arrays - Exercise/01. Count Chars in a String.cs
@@ -1,1 +1,33 @@
+namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
 
+            Dictionary<char, int> charsInts = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+                if (currentChar == ' ')
+                {
+                    continue;
+                }
+
+                if (!charsInts.ContainsKey(currentChar))
+                {
+                    charsInts.Add(currentChar, 0);
+                }
+
+                charsInts[currentChar]++;
+            }
+
+            foreach (KeyValuePair<char, int> kvp in charsInts)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
