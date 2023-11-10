e7d0060e183ea9aa68e49a0902da34f8d7216e50
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2023-11-10 22:46:03 +0200

    Update 01. Reverse Strings.cs

--- Fundamentals C#/Text Processing - Lab/01. Reverse Strings.cs
+++ Fundamentals C#/Text Processing - Lab/01. Reverse Strings.cs
@@ -1,1 +1,19 @@
+namespace _01._Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
 
+            while (input != "end")
+            {
+                char[] reversedWord = input.Reverse().ToArray();
+
+                Console.WriteLine($"{input} = {string.Join("", reversedWord)}");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
