2e17d62c52dca1833c6b65dd73a8a218e6e3d14e
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2023-12-28 00:47:49 +0200

    Update 03. Plant Discovery -> Dictionary.cs

--- Fundamentals C#/Past Exams/02. Programming Fundamentals Final Exam/03. Plant Discovery -> Dictionary.cs
+++ Fundamentals C#/Past Exams/02. Programming Fundamentals Final Exam/03. Plant Discovery -> Dictionary.cs
@@ -1,1 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalFeedingProgram
+{
+    class Plant
+    {
+        public string Name { get; set; }
+        public int Rarity { get; set; }
+        public List<int> Rating { get; set; }
+
+        public Plant(string name, int rarity)
+        {
+            Name = name;
+            Rarity = rarity;
+            Rating = new List<int>();
+        }
+
+        public override string ToString()
+        {
+            return $"- {Name}; Rarity: {Rarity}; Rating: {(Rating.Count == 0 ? 0 : Rating.Average()):f2}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Plant> plants = new Dictionary<string, Plant>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputs = Console.ReadLine().Split("<->");
+                string name = inputs[0];
+                int rarity;
+
+                if (!int.TryParse(inputs[1], out rarity))
+                {
+                    Console.WriteLine("error");
+                    continue;
+                }
+
+                if (!plants.ContainsKey(name))
+                {
+                    Plant plant = new Plant(name, rarity);
+                    plants.Add(name, plant);
+                }
+                else
+                {
+                    plants[name].Rarity = rarity;
+                }
+            }
+
+            string input;
+            while ((input = Console.ReadLine()) != "Exhibition")
+            {
+                string[] command = input.Split(": ");
+                if (command[0] == "Rate")
+                {
+                    string[] arguments = command[1].Split(" - ");
+                    string name = arguments[0];
+                    int rating;
+
+                    if (!int.TryParse(arguments[1], out rating))
+                    {
+                        Console.WriteLine("error");
+                        continue;
+                    }
+
+                    if (!plants.ContainsKey(name))
+                    {
+                        Console.WriteLine("error");
+                        continue;
+                    }
+
+                    plants[name].Rating.Add(rating);
+                }
+                else if (command[0] == "Update")
+                {
+                    string[] arguments = command[1].Split(" - ");
+                    string name = arguments[0];
+                    int newRarity;
+
+                    if (!int.TryParse(arguments[1], out newRarity))
+                    {
+                        Console.WriteLine("error");
+                        continue;
+                    }
+
+                    if (!plants.ContainsKey(name))
+                    {
+                        Console.WriteLine("error");
+                    }
+                    else
+                    {
+                        plants[name].Rarity = newRarity;
+                    }
+                }
+                else if (command[0] == "Reset")
+                {
+                    string name = command[1];
+                    if (!plants.ContainsKey(name))
+                    {
+                        Console.WriteLine("error");
+                    }
+                    else
+                    {
+                        plants[name].Rating.Clear();
+                    }
+                }
+            }
+
+            Console.WriteLine("Plants for the exhibition:");
+            foreach (var plant in plants)
+            {
+                Console.WriteLine(plant.Value);
+            }
+        }
+    }
+}
 
