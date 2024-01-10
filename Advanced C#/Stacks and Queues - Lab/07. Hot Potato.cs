3c59d50cdb44d622f7cf29b9980fff5d7db4e54e
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-10 20:44:14 +0200

    Update 07. Hot Potato.cs

--- Advanced C#/Stacks and Queues - Lab/07. Hot Potato.cs
+++ Advanced C#/Stacks and Queues - Lab/07. Hot Potato.cs
@@ -1,23 +1,29 @@
-
-
-Queue<string> children = new Queue<string>(Console.ReadLine()
-    .Split());
-
-int countThrow = int.Parse(Console.ReadLine());
-
-int count = 0;
-while (children.Count > 1)
+namespace _07.HotPotato
 {
-    count++;
-    string child = children.Dequeue();
-    if (count == countThrow)
+    internal class Program
     {
-        count = 0;
-        Console.WriteLine($"Removed {child}");
-    }
-    else
-    {
-        children.Enqueue(child);
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>(Console.ReadLine().Split());
+            int countThrow = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            while (queue.Count > 1)
+            {
+                count++;
+                string child = queue.Dequeue();
+                if (count == countThrow)
+                {
+                    Console.WriteLine($"Removed {child}");
+                    count = 0;
+                }
+                else
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
     }
 }
-Console.WriteLine($"Last is {children.Dequeue()}");
