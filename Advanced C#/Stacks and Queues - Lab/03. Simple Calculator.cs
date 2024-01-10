4b37ba5d16cc7a97f037138da19d40eb1f1e6f42
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-10 19:58:14 +0200

    Update 03. Simple Calculator.cs

--- Advanced C#/Stacks and Queues - Lab/03. Simple Calculator.cs
+++ Advanced C#/Stacks and Queues - Lab/03. Simple Calculator.cs
@@ -1,18 +1,30 @@
-Stack<string> stack = new Stack<string>(Console.ReadLine().Split().Reverse());
-
-int result = int.Parse(stack.Pop());
-
-while (stack.Count > 0)
+namespace _03.SimpleCalculator
 {
-    string operation = stack.Pop();
-    int secondNum = int.Parse(stack.Pop());
-    if (operation == "+")
+    internal class Program
     {
-        result += secondNum;
-    }
-    else if (operation == "-")
-    {
-        result -= secondNum;
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>(Console.ReadLine()
+                .Split().Reverse());
+
+            int result = int.Parse(stack.Pop());
+
+            while (stack.Count > 1)
+            {
+                string operation = stack.Pop();
+                int num2 = int.Parse(stack.Pop());
+
+                if (operation == "+")
+                {
+                    result += num2;
+                }
+                else if (operation == "-")
+                {
+                    result -= num2;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
     }
 }
-Console.WriteLine(result);
