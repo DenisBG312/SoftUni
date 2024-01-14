74d88cfc9f62cdc42dd421e15aa44b237d9e924e
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-14 18:04:34 +0200

    Create 02. Sum Matrix Columns.cs

---
+++ CSharp-Advanced-Jan-2024/Multidimensional Arrays - Lab/02. Sum Matrix Columns.cs
@@ -0,0 +1,35 @@
+namespace _2._Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputLine = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputLine[col];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
