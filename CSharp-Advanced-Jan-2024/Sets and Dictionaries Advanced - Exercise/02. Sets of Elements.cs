ac38c047d86dd7c4a494edf21e5f5a8f26fa7463
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-19 22:52:12 +0200

    Create 02. Sets of Elements.cs

---
+++ CSharp-Advanced-Jan-2024/Sets and Dictionaries Advanced - Exercise/02. Sets of Elements.cs
@@ -0,0 +1,38 @@
+using System.Globalization;
+
+namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int firstSetSize = inputs[0];
+            int secondSetSize = inputs[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < firstSetSize; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                firstSet.Add(num);
+            }
+
+            for (int i = 0; i < secondSetSize; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                secondSet.Add(num);
+            }
+
+            foreach (var num in firstSet)
+            {
+                if (secondSet.Contains(num))
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+    }
+}
