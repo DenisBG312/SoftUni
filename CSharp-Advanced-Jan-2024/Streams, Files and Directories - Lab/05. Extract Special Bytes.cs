9e2d3a763069802afc15478fea98e1191303655d
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-23 21:37:05 +0200

    Create 05. Extract Special Bytes.cs

---
+++ CSharp-Advanced-Jan-2024/Streams, Files and Directories - Lab/05. Extract Special Bytes.cs
@@ -0,0 +1,40 @@
+using System.Text;
+
+namespace ExtractSpecialBytes
+{
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            using StreamReader streamReader = new StreamReader(bytesFilePath);
+            byte[] fileBytes = File.ReadAllBytes(binaryFilePath);
+            List<string> bytesList = new List<string>();
+            StringBuilder sb = new StringBuilder();
+
+            while (!streamReader.EndOfStream)
+            {
+                bytesList.Add(streamReader.ReadLine());
+            }
+
+            foreach (var item in fileBytes)
+            {
+                if (bytesList.Contains(item.ToString()))
+                {
+                    sb.AppendLine(item.ToString());
+                }
+            }
+
+            using StreamWriter file = new StreamWriter(outputPath);
+            file.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
