938759d8affd2c9f059aa95c772b8be0597a94b5
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-25 23:00:47 +0200

    Update 01. Sort Even Numbers.cs

--- CSharp-Advanced-Jan-2024/Functional Programming - Lab/01. Sort Even Numbers.cs
+++ CSharp-Advanced-Jan-2024/Functional Programming - Lab/01. Sort Even Numbers.cs
@@ -1,1 +1,20 @@
+namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parseIt = s => int.Parse(s);
+            Func<int, bool> isEven = x => x % 2 == 0;
+
+            string input = Console.ReadLine();
+            string[] splitted = input.Split(", ");
+            int[] nums = splitted.Select(parseIt).ToArray();
+            int[] evenNums = nums.Where(isEven).ToArray();
+            int[] orderedNums = evenNums.OrderBy(x => x).ToArray();
+
+            Console.WriteLine(string.Join(", ", orderedNums));
+        }
+    }
+}
 
