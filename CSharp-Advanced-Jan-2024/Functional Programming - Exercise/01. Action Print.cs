c7a3b3d5179bf8816d13c49f8776ac722b232f26
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-27 23:59:33 +0200

    Update 01. Action Print.cs

--- CSharp-Advanced-Jan-2024/Functional Programming - Exercise/01. Action Print.cs
+++ CSharp-Advanced-Jan-2024/Functional Programming - Exercise/01. Action Print.cs
@@ -1,1 +1,18 @@
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> action = strings =>
+                Console.WriteLine(string.Join(Environment.NewLine, strings));
+
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.None)
+                .ToArray();
+
+            action(names);
+        }
+    }
+}
 
