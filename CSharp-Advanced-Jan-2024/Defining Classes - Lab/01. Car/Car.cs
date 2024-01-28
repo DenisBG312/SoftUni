3a9f78e455044425e95592ece372b66a0cc6c2a8
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-29 00:52:28 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Defining Classes - Lab/01. Car/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
