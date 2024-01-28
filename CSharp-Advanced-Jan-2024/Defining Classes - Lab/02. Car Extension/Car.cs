742ac76601c2c256aa9e7d8c9c68dc2f1777acf0
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-29 01:12:54 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Defining Classes - Lab/02. Car Extension/Car.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make
+        {
+            get
+            {
+                return make;
+            }
+            set
+            {
+                make = value;
+            }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get
+            {
+                return fuelConsumption;
+            }
+            set
+            {
+                fuelConsumption = value;
+            }
+        }
+
+        public void Drive(double distance)
+        {
+            double neededFuel = distance * fuelConsumption;
+            if (FuelQuantity - neededFuel >= 0)
+            {
+                FuelQuantity -= neededFuel;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {Make}");
+            sb.AppendLine($"Model: {Model}");
+            sb.AppendLine($"Year: {Year}");
+            sb.AppendLine($"Fuel: {FuelQuantity:f2}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CSharp-Advanced-Jan-2024/Defining Classes - Lab/02. Car Extension/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 500000;
+            car.FuelConsumption = 20;
+            car.Drive(2000);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
