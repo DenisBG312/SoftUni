300f872d61929e5c397104abd618aeb059ec4774
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-30 10:57:22 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person(20);
+            Person p2 = new Person("Denis", 17);
+            Person p3 = new Person();
+        }
+    }
+}
