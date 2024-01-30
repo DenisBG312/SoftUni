c63dd1245d4ff3fcb4874728842c3880fcefade3
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-30 11:37:04 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public static class DateModifier
+    {
+        public static void CalculateDifference(string startDate, string endDate)
+        {
+            DateTime startDateTime = DateTime.Parse(startDate);
+            DateTime endDateTime = DateTime.Parse(endDate);
+
+            TimeSpan difference = startDateTime - endDateTime;
+
+            double differenceDouble = Math.Abs(difference.TotalDays);
+
+            Console.WriteLine(differenceDouble);
+        } 
+    }
+}
---
+++ CSharp-Advanced-Jan-2024/Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            DateModifier.CalculateDifference(startDate, endDate);
+
+        }
+    }
+}
