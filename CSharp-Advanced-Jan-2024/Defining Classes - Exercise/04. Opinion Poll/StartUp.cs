c9ea2d98387cbb27f2f1ad68cd0f5b15939bb0fa
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-30 11:24:32 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] args = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = args[0];
+                int age = int.Parse(args[1]);
+
+                Person person = new Person(name, age);
+                people.Add(person);
+            }
+
+            List<Person> peopleSort = people.OrderBy(x => x.Name).ToList();
+
+            foreach (var person in peopleSort.Where(x => x.Age > 30))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");     
+            }
+
+        }
+    }
+}
