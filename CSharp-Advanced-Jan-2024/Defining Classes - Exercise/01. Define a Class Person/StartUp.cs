9cef5c71967d87e1304991976199ce0f213f4186
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-30 10:47:02 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Name = "Peter";
+            p1.Age = 20;
+
+            Person p2 = new Person();
+            p2.Name = "George";
+            p2.Age = 18;
+
+            Person p3 = new Person();
+            p3.Name = "Jose";
+            p3.Age = 43;
+        }
+    }
+}
