b2c57694903686a014ddb331166e15e3003a5430
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-30 21:40:11 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Defining Classes - Exercise/08. Car Salesman/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._Speed_Racing
+{
+    public class Car
+    {
+        public Car(string model, Engine engine)
+        {
+            Model = model;
+            Engine = engine;
+        }
+
+        public Car(string model, Engine engine, int weight, string color) : this(model, engine)
+        {
+            Weight = weight;
+            Color = color;
+        }
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public int Weight { get; set; }
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            string weight = Weight == 0 ? "n/a" : Weight.ToString();
+            string color = Color ?? "n/a";
+
+
+            StringBuilder sb = new();
+
+            sb.AppendLine($"{Model}:");
+            sb.AppendLine($"  {Engine.ToString()}");
+            sb.AppendLine($"  Weight: {weight}");
+            sb.AppendLine($"  Color: {color}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp-Advanced-Jan-2024/Defining Classes - Exercise/08. Car Salesman/StartUp.cs
@@ -0,0 +1,97 @@
+﻿
+
+using _06._Speed_Racing;
+
+namespace DefineClasses;
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int numberOfEngines = int.Parse(Console.ReadLine());
+        List<Car> cars = new List<Car>();
+        List<Engine> engines = new List<Engine>();
+
+        for (int i = 0; i < numberOfEngines; i++)
+        {
+            string[] engineProperties = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Engine engine = CreateEngine(engineProperties);
+
+            engines.Add(engine);
+        }
+
+        int numberOfCars = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfCars; i++)
+        {
+            string[] carProperties = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Car car = CreateCar(carProperties, engines);
+
+            cars.Add(car);
+        }
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car.ToString());
+        }
+    }
+
+    static Engine CreateEngine(string[] enginePropeties)
+    {
+        Engine engine = new(enginePropeties[0], int.Parse(enginePropeties[1]));
+
+        if (enginePropeties.Length > 2)
+        {
+            int displacement;
+
+            bool isDigit = int.TryParse(enginePropeties[2], out displacement);
+
+            if (isDigit)
+            {
+                engine.Displacement = displacement;
+            }
+            else
+            {
+                engine.Efficiency = enginePropeties[2];
+            }
+
+            if (enginePropeties.Length > 3)
+            {
+                engine.Efficiency = enginePropeties[3];
+            }
+        }
+
+        return engine;
+    }
+
+    static Car CreateCar(string[] carPropeties, List<Engine> engines)
+    {
+        Engine engine = engines.Find(x => x.Model == carPropeties[1]);
+
+        Car car = new(carPropeties[0], engine);
+
+        if (carPropeties.Length > 2)
+        {
+            int weight;
+
+            bool isDigit = int.TryParse(carPropeties[2], out weight);
+
+            if (isDigit)
+            {
+                car.Weight = weight;
+            }
+            else
+            {
+                car.Color = carPropeties[2];
+            }
+
+            if (carPropeties.Length > 3)
+            {
+                car.Color = carPropeties[3];
+            }
+        }
+
+        return car;
+    }
+}
