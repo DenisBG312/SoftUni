e12b12a7591f4a9ebd4292c0f542cb9d74b2e2ce
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-01-30 11:13:07 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,22 @@
+﻿namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            people = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.people.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return people.MaxBy(x => x.Age);
+        }
+    }
+}
