1d87bb77d034612bebf28f51ead103d9c73647cc
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-01 22:09:30 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Creating Custom Doubly Linked List/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Doubly_Linked_List
+{
+    public class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+        public int Value { get; set; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+    }
+}
---
+++ CSharp-Advanced-Jan-2024/Creating Custom Doubly Linked List/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Custom_Doubly_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+            linkedList.AddLast(4);
+            linkedList.AddFirst(0);
+            linkedList.AddFirst(-1);
+
+            linkedList.RemoveFirst();
+            linkedList.RemoveLast();
+
+            linkedList.ForEach(n =>
+            {
+                Console.WriteLine(n);
+            });
+
+            int[] array = linkedList.ToArray();
+
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
