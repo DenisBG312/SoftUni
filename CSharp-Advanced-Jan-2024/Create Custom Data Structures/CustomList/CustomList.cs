8b8d064b3db5b855f8283d26de336a29c8c13c06
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-04 16:32:07 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Create Custom Data Structures/CustomList/CustomList.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.CustomList
+{
+    public class CustomList
+    {
+        private const int InitialCapacity = 2;
+        private int[] items;
+
+        public CustomList()
+        {
+            items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                ThrowExceptionIfIndexOutOfRange(index);
+                return items[index];
+            }
+            set
+            {
+                ThrowExceptionIfIndexOutOfRange(index);
+                items[index] = value;
+            }
+        }
+
+
+        public void Add(int item)
+        {
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+            items[Count] = item;
+
+            Count++;
+        }
+        public int RemoveAt(int index)
+        {
+            ThrowExceptionIfIndexOutOfRange(index);
+
+            int removedItem = items[index];
+
+            items[index] = default;
+
+            ShiftLeft(index);
+
+            Count--;
+
+            if (Count <= items.Length / 4)
+            {
+                Shrink();
+            }
+            return removedItem;
+        }
+
+        public void InsertAt(int index, int item)
+        {
+            ThrowExceptionIfIndexOutOfRange(index);
+
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+
+            ShiftRight(index);
+            items[index] = item;
+
+            Count++;
+        }
+
+        public bool Contains(int item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i] == item)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void ShiftRight(int index)
+        {
+            for (int i = Count - 1; i >= index; i--)
+            {
+                items[i + 1] = items[i];
+            }
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            items[Count - 1] = default;
+        }
+        private void Resize()
+        {
+            int[] copy = new int[items.Length * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+        private void ThrowExceptionIfIndexOutOfRange(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException("Index is invalid");
+            }
+        }
+        private void Shrink()
+        {
+            int[] copy = new int[items.Length / 2];
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+    }
+}
---
+++ CSharp-Advanced-Jan-2024/Create Custom Data Structures/CustomList/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _01.CustomList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList list = new CustomList();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+ 
+
+            list[0] = 234;
+
+
+            Console.WriteLine(list[0]);
+
+            //list.AddRange(new int[] { 1, 2, 3, 4 });
+
+            Console.WriteLine(list.RemoveAt(2));
+            Console.WriteLine(list.RemoveAt(2));
+            Console.WriteLine(list.RemoveAt(2));
+
+
+            list.InsertAt(2, -5);
+            //Console.WriteLine();
+            //
+            Console.WriteLine(list.Contains(234));
+            Console.WriteLine(list.Contains(100));
+            //
+            //list.InsertAt(0, -5);
+            //
+            //list.Swap(0, 1);
+            //Console.WriteLine();
+            //
+            //Console.WriteLine();
+            Console.WriteLine(list.Count);
+        }
+    }
+}
