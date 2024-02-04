0cfb78157d2acfb332313ab4fb1d3fadece93219
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-04 17:32:39 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Create Custom Data Structures/CustomStack/CustomStack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CustomStack
+{
+    public class CustomStack
+    {
+        private const int InitialCapacity = 4;
+        private int[] items;
+
+        public CustomStack()
+        {
+            items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(int item)
+        {
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+
+            items[Count] = item;
+
+            Count++;
+        }
+
+        public int Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty");
+            }
+            int removedItem = items[Count - 1];
+
+            items[Count - 1] = default;
+
+            Count--;
+            return removedItem;
+        }
+        public int Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty");
+            }
+
+            return items[Count - 1];
+        }
+        private void Resize()
+        {
+            int[] copy = new int[items.Length * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                int currentItem = items[i];
+
+                action(currentItem);
+            }
+        }
+    }
+}
