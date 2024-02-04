2c79fc58d4c318ab0513c0278cceff81874bc642
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-04 18:06:03 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Create Custom Data Structures/CustomQueue/CustomQueue.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.CustomQueue
+{
+    public class CustomQueue
+    {
+        private const int InitialCapacity = 8;
+        private const int FirstElementIndex = 0;
+
+        private int[] items;
+
+        public CustomQueue()
+        {
+            items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Enqueue(int item)
+        {
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+            items[Count] = item;
+
+            Count++;
+        }
+
+        public int Dequeue()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("CustomQueue is empty");
+            }
+
+            int removedItem = items[FirstElementIndex];
+            ShiftLeft();
+            Count--;
+            return removedItem;
+        }
+        public int Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty");
+            }
+
+            return items[FirstElementIndex];
+        }
+
+        public void Clear()
+        {
+            items = new int[InitialCapacity];
+
+            Count = 0;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                int currentItem = items[i];
+
+                action(currentItem);
+            }
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[items.Length * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+        private void ShiftLeft()
+        {
+            for (int i = FirstElementIndex; i < Count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+        }
+    }
+}
