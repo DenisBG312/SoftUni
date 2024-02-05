c6c6b2cbeff366827fed040d90cbc8111b3e0aca
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-05 15:25:11 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Generics - Lab/02. Array Creator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericArrayCreator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", ArrayCreator.Create(5, "Denis")));
+        }
+    }
+}
