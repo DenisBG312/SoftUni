a403ed1094d162d9be8ce44b08ff6581cbde1e56
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-05 21:51:16 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Generics - Lab/03. Generic Scale/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericScale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> notEqualScale = new EqualityScale<int>(5, 6);
+            Console.WriteLine(notEqualScale.AreEqual());
+
+            EqualityScale<int> equalScale = new EqualityScale<int>(3, 3);
+            Console.WriteLine(equalScale.AreEqual());
+        }
+    }
+}
