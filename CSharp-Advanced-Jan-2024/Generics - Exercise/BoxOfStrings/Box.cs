ed4e3c6859de296a87f5621cdc6a1aa0d07cc1d4
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-06 22:18:12 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Generics - Exercise/BoxOfStrings/Box.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace BoxOfStrings
+{
+    public class Box<T>
+    {
+        private List<T> items = new List<T>();
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString();
+        }
+    }
+}
