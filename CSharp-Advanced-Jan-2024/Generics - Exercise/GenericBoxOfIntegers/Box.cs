7c5ac3c019e6d19d5dc18da7b7817947570f0212
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-07 18:14:55 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Generics - Exercise/GenericBoxOfIntegers/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfIntegers
+{
+    public class Box<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ CSharp-Advanced-Jan-2024/Generics - Exercise/GenericBoxOfIntegers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GenericBoxOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(input);
+
+                Console.WriteLine($"{box.Value.GetType()}: {box}");
+            }
+        }
+    }
+}
