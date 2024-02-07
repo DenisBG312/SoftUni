b41a3a4bdeeae5af04fa1e3cb0b6499c817ba1c9
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-07 18:47:26 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Generics - Exercise/GenericCountMethodString/Box.cs
@@ -0,0 +1,32 @@
+﻿namespace GenericCountMethodString
+{
+    public class Box<T> where T: IComparable<T>
+
+    {
+    private List<T> list;
+
+    public Box()
+    {
+        list = new List<T>();
+    }
+
+    public void Add(T item)
+    {
+        list.Add(item);
+    }
+
+    public int Count(T itemToCompare)
+    {
+        int count = 0;
+        foreach (var item in list)
+        {
+            if (item.CompareTo(itemToCompare) > 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+    }
+}
---
+++ CSharp-Advanced-Jan-2024/Generics - Exercise/GenericCountMethodString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericCountMethodString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                box.Add(input);
+            }
+
+            string itemToCompare = Console.ReadLine();
+
+            Console.WriteLine(box.Count(itemToCompare));
+        }
+    }
+}
