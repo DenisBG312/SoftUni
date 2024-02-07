3f4d0161ff3092c8eb0e566bdae62bf02ead298e
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-07 18:52:04 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Generics - Exercise/GenericCountMethodDouble/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericCountMethodString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+
+                box.Add(input);
+            }
+
+            double itemToCompare = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(box.Count(itemToCompare));
+        }
+    }
+}
