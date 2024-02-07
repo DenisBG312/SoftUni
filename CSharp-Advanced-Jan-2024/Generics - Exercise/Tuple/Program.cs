2cf19c00a0b87801b949a806aa3123b9f3501269
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-07 19:06:38 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Generics - Exercise/Tuple/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Tuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputOne = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string[] inputTwo = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string[] inputThree = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+
+
+            CustomTuple<string, string> tuple = 
+                new CustomTuple<string, string>(inputOne[0] + " " + inputOne[1], inputOne[2]);
+
+            CustomTuple<string, int> tupleTwo = 
+                new CustomTuple<string, int>(inputTwo[0], int.Parse(inputTwo[1]));
+
+            CustomTuple<int, double> tupleThree =
+                new CustomTuple<int, double>(int.Parse(inputThree[0]), double.Parse(inputThree[1]));
+
+            Console.WriteLine(tuple);
+            Console.WriteLine(tupleTwo);
+            Console.WriteLine(tupleThree);
+        }
+    }
+}
