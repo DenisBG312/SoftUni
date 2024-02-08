afdf994020884e55e5d1d6a4493d4201d6c46fb0
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-08 21:56:44 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Iterators and Comparators - Lab/02. LibraryIterator/Library.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private int index = -1;
+            private List<Book> books;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+            }
+
+            public Book Current => books[index];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                return ++index < books.Count;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+        }
+    }
+}
