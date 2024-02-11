8d2f8c41a5a2e8f5ebffb6d792b56aa088e3424d
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-11 19:40:11 +0200

    Update 01. Temple of Doom.cs

--- CSharp-Advanced-Jan-2024/C# Advanced Regular Exam - 17 June 2023/01. Temple of Doom.cs
+++ CSharp-Advanced-Jan-2024/C# Advanced Regular Exam - 17 June 2023/01. Temple of Doom.cs
@@ -1,1 +1,78 @@
+namespace _01._Temple_of_Doom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] toolsArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] substancesArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            List<int> challenges = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Queue<int> tools = new Queue<int>(toolsArr);
+            Stack<int> substances = new Stack<int>(substancesArr);
+
+            while (true)
+            {
+                int tool = tools.Peek();
+                int substance = substances.Peek();
+
+                if (challenges.Contains(tool * substance))
+                {
+                    challenges.Remove(tool * substance);
+                    tools.Dequeue();
+                    substances.Pop();
+
+                    if (challenges.Count == 0)
+                    {
+                        Console.WriteLine("Harry found an ostracon, which is dated to the 6th century BCE.");
+                        break;
+                    }
+                }
+
+                else
+                {
+                    tools.Enqueue(tools.Dequeue() + 1);
+                    substances.Push(substances.Pop() - 1);
+
+                    if (substances.Peek() == 0)
+                    {
+                        substances.Pop();
+                    }
+
+                    if (substances.Count == 0)
+                    {
+                        Console.WriteLine("Harry is lost in the temple. Oblivion awaits him.");
+                        break;
+                    }
+                }
+            }
+
+            if (substances.Count != 0)
+            {
+                Console.WriteLine($"Substances: {string.Join(", ", substances)}");
+            }
+
+            if (tools.Count != 0)
+            {
+                Console.WriteLine($"Tools: {string.Join(", ", tools)}");
+            }
+
+            if (challenges.Count != 0)
+            {
+                Console.WriteLine($"Challenges: {string.Join(", ", challenges)}");
+            }
+        }
+    }
+}
 
