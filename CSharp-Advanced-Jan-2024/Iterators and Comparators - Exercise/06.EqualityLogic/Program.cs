54a65bac093380823df194f3fb3f59762b366e3a
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-13 23:55:51 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Iterators and Comparators - Exercise/06.EqualityLogic/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _06.EqualityLogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Person> hashSet = new HashSet<Person>();
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] personProps = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person
+                {
+                    Name = personProps[0],
+                    Age = int.Parse(personProps[1])
+                };
+
+                person.Equals(5);
+
+
+                hashSet.Add(person);
+                sortedSet.Add(person);
+
+            }
+
+            Console.WriteLine(hashSet.Count);
+            Console.WriteLine(sortedSet.Count);
+        }
+    }
+}
