ae961153365efac841dcc0f0481280945ae98a7d
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-13 21:45:15 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Iterators and Comparators - Exercise/03.Stack/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _03.Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<int> customStack = new CustomStack<int>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] tokens = command
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string action = tokens[0];
+
+                if (action == "Push")
+                {
+                    int[] itemsToPush = tokens
+                        .Skip(1)
+                        .Select(int.Parse)
+                        .ToArray();
+
+                    foreach (var item in itemsToPush)
+                    {
+                        customStack.Push(item);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        customStack.Pop();
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            foreach (var item in customStack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in customStack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
