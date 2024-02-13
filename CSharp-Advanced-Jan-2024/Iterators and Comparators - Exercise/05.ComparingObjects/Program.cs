0119dddae488aafb11072ad2076687a69233d002
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-13 22:20:12 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Iterators and Comparators - Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _05.ComparingObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] personProps = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person
+                {
+                    Name = personProps[0],
+                    Age = int.Parse(personProps[1]),
+                    Town = personProps[2]
+                };
+
+                people.Add(person);
+            }
+
+            int compareIndex = int.Parse(Console.ReadLine()) - 1;
+
+            Person personToCompare = people[compareIndex];
+
+            int equalCount = 0;
+            int diffCount = 0;
+            
+            foreach (Person person in people)
+            {
+                if (person.CompareTo(personToCompare) == 0)
+                {
+                    equalCount++;
+                }
+                else
+                {
+                    diffCount++;
+                }
+            }
+
+            if (equalCount == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equalCount} {diffCount} {people.Count}");
+            }
+        }
+    }
+}
