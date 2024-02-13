bad354b5cd6c86d82a4d2e1320a7e8267049a3b8
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-13 21:59:12 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Iterators and Comparators - Exercise/04.Froggy/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.Froggy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> stones = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Lake lake = new Lake(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
