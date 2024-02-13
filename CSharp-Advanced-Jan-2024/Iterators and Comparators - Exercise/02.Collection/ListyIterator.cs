4bf31d537720196feeda25a45fdbfdafbe1af992
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-13 21:18:22 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/Iterators and Comparators - Exercise/02.Collection/ListyIterator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private int index;
+        private List<T> items;
+
+        public ListyIterator(List<T> items)
+        {
+            this.items = items;
+        }
+
+        public bool Move()
+        {
+            if (index < items.Count - 1)
+            {
+                index++;
+
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index < items.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(items[index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ CSharp-Advanced-Jan-2024/Iterators and Comparators - Exercise/02.Collection/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _01._ListyIterator;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> items = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Skip(1)
+            .ToList();
+
+        ListyIterator<string> listyIterator = new ListyIterator<string>(items);
+
+        string command;
+        while ((command = Console.ReadLine()) != "END")
+        {
+            switch (command)
+            {
+                case "Move":
+                    Console.WriteLine(listyIterator.Move());
+                    break;
+                case "HasNext":
+                    Console.WriteLine(listyIterator.HasNext());
+                    break;
+                case "Print":
+                    try
+                    {
+                        listyIterator.Print();
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                case "PrintAll":
+                    foreach (var item in listyIterator)
+                    {
+                        Console.Write($"{item} ");
+                    }
+
+                    Console.WriteLine();
+
+                    break;
+            }
+        }
+    }
+}
