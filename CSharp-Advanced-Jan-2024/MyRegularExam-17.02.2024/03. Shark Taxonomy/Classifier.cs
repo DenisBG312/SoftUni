7fad2c96ee27ea68bdd61156a9a82127332557af
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-17 13:28:29 +0200

    Add files via upload

---
+++ CSharp-Advanced-Jan-2024/MyRegularExam-17.02.2024/03. Shark Taxonomy/Classifier.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+namespace SharkTaxonomy
+{
+    public class Classifier
+    {
+        public int Capacity { get; set; }
+        public List<Shark> Species { get; set; }
+
+        public int GetCount => Species.Count;
+
+        public Classifier(int capacity)
+        {
+            Capacity = capacity;
+            Species = new List<Shark>();
+        }
+
+        public void AddShark(Shark shark)
+        {
+            if (Capacity > Species.Count)
+            {
+                if (!Species.Any(x => x.Kind == shark.Kind))
+                {
+                    Species.Add(shark);
+                }
+            }
+        }
+
+        public bool RemoveShark(string kind)
+        {
+            Shark shark = Species.FirstOrDefault(x => x.Kind == kind);
+            if (shark != null)
+            {
+                Species.Remove(shark);
+                return true;
+            }
+
+            return false;
+        }
+
+        public string GetLargestShark()
+        {
+            Shark longestShark = Species.OrderByDescending(x => x.Length).First();
+
+            return longestShark.ToString().TrimEnd();
+        }
+
+        public double GetAverageLength()
+        {
+            double average = Species.Average(x => x.Length);
+
+            return average;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Species.Count} sharks classified:");
+            foreach (var shark in Species)
+            {
+                sb.AppendLine(shark.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp-Advanced-Jan-2024/MyRegularExam-17.02.2024/03. Shark Taxonomy/Shark.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace SharkTaxonomy
+{
+    public class Shark
+    {
+        public string Kind { get; set; }
+        public int Length { get; set; }
+        public string Food { get; set; }
+        public string Habitat { get; set; }
+
+        public Shark(string kind, int length, string food, string habitat)
+        {
+            Kind = kind;
+            Length = length;
+            Food = food;
+            Habitat = habitat;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Kind} shark: {Length}m long.");
+            sb.AppendLine($"Could be spotted in the {Habitat}, typical menu: {Food}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
