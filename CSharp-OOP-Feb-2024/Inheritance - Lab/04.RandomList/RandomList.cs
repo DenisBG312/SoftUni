cc6498e00c3f45ff6cb61943e74a3808f8bc248e
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-22 21:50:38 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Inheritance - Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomRandomList;
+
+public class RandomList : List<string>
+{
+    public string RandomString()
+    {
+        Random random = new Random();
+
+        string item = this[random.Next(0, this.Count)];
+        this.Remove(item);
+        return item;
+
+    }
+}
---
+++ CSharp-OOP-Feb-2024/Inheritance - Lab/04.RandomList/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+            
+            randomList.Add("2");
+            randomList.Add("3");
+            randomList.Add("4");
+
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
