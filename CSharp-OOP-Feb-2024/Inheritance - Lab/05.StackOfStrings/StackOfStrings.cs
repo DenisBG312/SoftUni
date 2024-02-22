3e050bf0b0fcac38aa2f7de40bceedd79148e130
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-22 22:16:11 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Inheritance - Lab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomStack;
+
+public class StackOfStrings : Stack<string>
+{
+    public bool IsEmpty()
+    {
+        if (this.Count == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void AddRange(List<string> list)
+    {
+        foreach (var element in list)
+        { 
+            this.Push(element);
+        }
+    }
+}
---
+++ CSharp-OOP-Feb-2024/Inheritance - Lab/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+
+            stackOfStrings.Push("2");
+            stackOfStrings.Push("3");
+            stackOfStrings.Push("4");
+
+            Console.WriteLine(stackOfStrings.IsEmpty());
+
+            stackOfStrings.AddRange(new List<string>{"2", "2", "10"});
+
+            foreach (var stackOfString in stackOfStrings)
+            {
+                Console.WriteLine(stackOfString);
+            }
+        }
+    }
+}
