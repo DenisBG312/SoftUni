37b01f8d6928d1b7b8439a42784b802bde4322be
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-24 19:37:05 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace Person;
+
+public class Person
+{
+    private int _age;
+    private string _name;
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public virtual int Age
+    {
+        get => _age;
+        set
+        {
+            if (value >= 0)
+            {
+                _age = value;
+            }
+        }   
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Age: {Age}";
+    }
+}
