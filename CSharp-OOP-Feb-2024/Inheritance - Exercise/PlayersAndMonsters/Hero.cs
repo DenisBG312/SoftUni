a7e266d7b02a6d62cc5509ff708d5a50a4cd8e1d
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-24 23:49:34 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Inheritance - Exercise/PlayersAndMonsters/Hero.cs
@@ -0,0 +1,30 @@
+﻿namespace PlayersAndMonsters;
+
+public class Hero
+{
+    private string username;
+    private int level;
+
+    public Hero(string username, int level)
+    {
+        Username = username;
+        Level = level;
+    }
+
+    public string Username
+    {
+        get => username;
+        set => username = value;
+    }
+
+    public int Level
+    {
+        get => level;
+        set => level = value;
+    }
+
+    public override string ToString()
+    {
+        return $"Type: {GetType().Name} Username: {Username} Level: {Level}";
+    }
+}
