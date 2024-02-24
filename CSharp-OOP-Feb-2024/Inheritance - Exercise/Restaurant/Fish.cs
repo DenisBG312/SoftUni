ccca3f184e25968e7ddb4e177cd67ce66ea3c291
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-25 00:36:34 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Inheritance - Exercise/Restaurant/Fish.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurant;
+
+public class Fish : MainDish
+{
+    private const double Grams = 22;
+    public Fish(string name, decimal price) : base(name, price, Grams)
+    {
+    }
+}
