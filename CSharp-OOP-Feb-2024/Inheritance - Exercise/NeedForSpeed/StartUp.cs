f8c94bef9b547d013feab77f1982ab503b70c827
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-02-25 00:12:46 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Inheritance - Exercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SportCar car = new SportCar(200, 300);
+            car.Drive(20);
+            Console.WriteLine(car.Fuel);
+        }
+    }
+}
