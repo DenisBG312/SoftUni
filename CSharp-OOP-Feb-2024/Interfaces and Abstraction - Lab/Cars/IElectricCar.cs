d56cec04dbd7f218e3682683cf3ca49c6dc77726
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-03-01 21:40:57 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Interfaces and Abstraction - Lab/Cars/IElectricCar.cs
@@ -0,0 +1,6 @@
+﻿namespace Cars;
+
+public interface IElectricCar
+{
+    public int Battery { get; set; }
+}
---
+++ CSharp-OOP-Feb-2024/Interfaces and Abstraction - Lab/Cars/Tesla.cs
@@ -0,0 +1,30 @@
+﻿namespace Cars;
+
+public class Tesla : ICar, IElectricCar
+{
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int Battery { get; set; }
+
+    public Tesla(string model, string color, int battery)
+    {
+        Model = model;
+        Color = color;
+        Battery = battery;
+    }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        return $"{Color} {nameof(Tesla)} {Model} with {Battery} Batteries";
+    }
+}
