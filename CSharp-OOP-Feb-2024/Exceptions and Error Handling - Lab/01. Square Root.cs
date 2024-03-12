27ac54bbe01dfae961d84c7569fd26e1cbf92801
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-03-12 22:00:23 +0200

    Update 01. Square Root.cs

--- CSharp-OOP-Feb-2024/Exceptions and Error Handling - Lab/01. Square Root.cs
+++ CSharp-OOP-Feb-2024/Exceptions and Error Handling - Lab/01. Square Root.cs
@@ -1,1 +1,29 @@
+namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
 
