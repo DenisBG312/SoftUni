2065c7755dc70cc372f609e8bc0f797979eabf08
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-03-17 00:04:26 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Reflection and Attributes - Lab/01. Stealer/Spy.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer;
+
+public class Spy
+{
+    public string StealFieldInfo(string className,params string[] fieldsToInvestigate)
+    {
+        Type type = Type.GetType(className);
+        FieldInfo[] classFields = type.GetFields(BindingFlags.Instance | BindingFlags.Static |
+                                                 BindingFlags.NonPublic | BindingFlags.Public);
+        StringBuilder sb = new StringBuilder();
+
+        Object classInstance = Activator.CreateInstance(type, new object[] { });
+        sb.AppendLine($"Class under investigation: {className}");
+
+        foreach (FieldInfo fieldInfo in classFields.Where(f => fieldsToInvestigate.Contains(f.Name)))
+        {
+            sb.AppendLine($"{fieldInfo.Name} = {fieldInfo.GetValue(classInstance)}");
+        }
+
+        return sb.ToString().Trim();
+
+    }
+}
