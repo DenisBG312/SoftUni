9aef983a12e38d08c2bb9ca1accbd031a6436260
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-03-17 01:28:47 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Reflection and Attributes - Lab/04. Collector/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer;
+
+public class Spy
+{
+    public string CollectGettersAndSetters(string className)
+    {
+        Type classType = Type.GetType(className);
+        MethodInfo[] classMethods =
+            classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        StringBuilder sb = new StringBuilder();
+
+        foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("get")))
+        {
+            sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+        }
+
+        foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("set")))
+        {
+            sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+        }
+
+        return sb.ToString().Trim();
+    }
+}
