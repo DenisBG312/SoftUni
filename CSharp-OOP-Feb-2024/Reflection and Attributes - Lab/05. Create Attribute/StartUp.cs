d07f16476ca7139d2e546c30ce498ff7a1c5854d
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-03-17 01:37:16 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Reflection and Attributes - Lab/05. Create Attribute/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace _05._Create_Attribute
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
