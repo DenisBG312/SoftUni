b906d773371a34ffd60e67090dc888ac801ce99a
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-03-17 00:22:41 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Reflection and Attributes - Lab/03. Mission Private Impossible/Spy.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer;
+
+public class Spy
+{
+    public string RevealPrivateMethods(string className)
+    {
+        Type classType = Type.GetType(className);
+        MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"All private Methods of Class: {className}");
+        sb.AppendLine($"Base class: {classType.BaseType.Name}");
+
+        foreach (MethodInfo classMethod in classMethods)
+        {
+            sb.AppendLine(classMethod.Name);
+        }
+
+        return sb.ToString().Trim();
+    }
+}
