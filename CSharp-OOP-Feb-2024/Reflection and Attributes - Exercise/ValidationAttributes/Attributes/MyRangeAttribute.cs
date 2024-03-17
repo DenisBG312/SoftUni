61c6a6c09a4b8c882198ecb636306c08b9969885
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-03-17 13:50:36 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Reflection and Attributes - Exercise/ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int minValue;
+        private int maxValue;
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+        public override bool IsValid(object obj) 
+            => (int)obj >= minValue && (int)obj <= maxValue; //TODO may need change
+    }
+}
---
+++ CSharp-OOP-Feb-2024/Reflection and Attributes - Exercise/ValidationAttributes/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ValidationAttributes.Models;
+using Validator = ValidationAttributes.Utils.Validator;
+
+namespace ValidationAttributes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person
+             (
+                 "asd",
+                 15
+             );
+
+            bool isValidEntity = Validator.IsValid(person);
+
+            Console.WriteLine(isValidEntity);
+        }
+    }
+}
