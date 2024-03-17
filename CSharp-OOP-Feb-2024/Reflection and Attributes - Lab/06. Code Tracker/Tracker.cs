deb01f35f4e114603729987bfd78f3c660449b75
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-03-17 02:02:30 +0200

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/Reflection and Attributes - Lab/06. Code Tracker/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace AuthorProblem;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        PrintMethodsByType(typeof(StartUp));
+    }
+
+    private void PrintMethodsByType(Type type)
+    {
+        MethodInfo[] methods = type.GetMethods();
+        foreach (MethodInfo method in methods)
+        {
+            List<AuthorAttribute> attributes =
+                type.GetCustomAttributes<AuthorAttribute>().ToList();
+            if (attributes.Count > 0)
+            {
+                foreach (var authorAttribute in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {authorAttribute.Name}");
+                }
+            }
+        }
+    }
+}
