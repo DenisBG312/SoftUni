587ed781a21ed0f2f388d4af2463e93a3b8dc5b8
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-03-31 23:08:48 +0300

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/PastExams/C# OOP Regular Exam - 5 August 2023/BankLoan/Repositories/BankRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankLoan.Models.Contracts;
+using BankLoan.Repositories.Contracts;
+
+namespace BankLoan.Repositories
+{
+    public class BankRepository : IRepository<IBank>
+    {
+        private List<IBank> models;
+
+        public BankRepository()
+        {
+            models = new List<IBank>();
+        }
+        public IReadOnlyCollection<IBank> Models => models.AsReadOnly();
+        public void AddModel(IBank model)
+        {
+            models.Add(model);
+        }
+
+        public bool RemoveModel(IBank model)
+        {
+            return models.Remove(model);
+        }
+
+        public IBank FirstModel(string name)
+        {
+            return models.FirstOrDefault(b => b.GetType().Name == name);
+        }
+    }
+}
