bd76d3e0a45d455c3ea627cfbaaeb64999fba171
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-04-03 00:12:15 +0300

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/PastExams/C# OOP Retake Exam – 18 April 2023/Structure/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EDriveRent.Models.Contracts;
+using EDriveRent.Repositories.Contracts;
+
+namespace EDriveRent.Repositories
+{
+    public class UserRepository : IRepository<IUser>
+    {
+        private List<IUser> users;
+
+        public UserRepository()
+        {
+            users = new List<IUser>();
+        }
+        public void AddModel(IUser model)
+        {
+            users.Add(model);
+        }
+
+        public bool RemoveById(string identifier)
+        {
+            IUser user = users.FirstOrDefault(x => x.DrivingLicenseNumber == identifier);
+            if (user != null)
+            {
+                users.Remove(user);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IUser FindById(string identifier)
+        {
+            return users.FirstOrDefault(d => d.DrivingLicenseNumber == identifier);
+        }
+
+        public IReadOnlyCollection<IUser> GetAll() => users.AsReadOnly();
+    }
+}
