aa292e0cce2d9599ad01c47026ddcf329182f4dc
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-04-03 17:41:52 +0300

    Add files via upload

---
+++ CSharp-OOP-Feb-2024/PastExams/C# OOP Retake Exam – 18 April 2023/Structure/Models/User.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EDriveRent.Models.Contracts;
+using EDriveRent.Utilities.Messages;
+
+namespace EDriveRent.Models
+{
+    public class User : IUser
+    {
+        private string firstName;
+        private string lastName;
+        private string drivingLicenseNumber;
+
+        public User(string firstName, string lastName, string drivingLicenseNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Rating = 0;
+            DrivingLicenseNumber = drivingLicenseNumber;
+            IsBlocked = false;
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.FirstNameNull);
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.LastNameNull);
+                }
+                lastName = value;
+            }
+        }
+
+        public double Rating { get; private set; }
+
+        public string DrivingLicenseNumber
+        {
+            get => drivingLicenseNumber;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.DrivingLicenseRequired);
+                }
+                drivingLicenseNumber = value;
+            }
+        }
+
+        public bool IsBlocked { get; private set; }
+        public void IncreaseRating()
+        {
+            Rating += 0.5;
+
+            if (Rating > 10)
+            {
+                Rating = 10;
+            }
+        }
+
+        public void DecreaseRating()
+        {
+            Rating -= 2;
+            if (Rating < 0)
+            {
+                Rating = 0;
+                IsBlocked = true;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} Driving license: {drivingLicenseNumber} Rating: {Rating}";
+        }
+    }
+}
