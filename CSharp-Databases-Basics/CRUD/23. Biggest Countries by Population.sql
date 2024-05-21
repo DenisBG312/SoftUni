d11de03b34d081f21bcccfcb767e2853ea05ad38
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-05-21 17:10:36 +0300

    Create 23. Biggest Countries by Population.sql

---
+++ CSharp-Databases-Basics/CRUD/23. Biggest Countries by Population.sql
@@ -0,0 +1,14 @@
+USE [Geography]
+
+GO
+
+SELECT * FROM [Countries]
+
+     SELECT 
+        TOP (30) [CountryName], [Population]
+       FROM [Countries]
+	  WHERE [ContinentCode] = 'EU'
+   ORDER BY [Population] DESC, 
+			[CountryName]
+  
+
