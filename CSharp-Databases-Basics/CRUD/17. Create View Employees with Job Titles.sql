7daf60b845ab87acdf5914e0298f9c3807896a8b
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-05-21 16:37:14 +0300

    Create 17. Create View Employees with Job Titles.sql

---
+++ CSharp-Databases-Basics/CRUD/17. Create View Employees with Job Titles.sql
@@ -0,0 +1,11 @@
+USE SoftUni
+
+GO
+
+CREATE VIEW [V_EmployeeNameJobTitle] AS
+	 SELECT CONCAT([FirstName], ' ', [MiddleName], ' ', [LastName])
+	     AS [Full Name],
+	        [JobTitle]
+	   FROM Employees
+
+
