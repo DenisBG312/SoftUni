b4ce3f7bcee1b029a1e3e7eb331c4dd2120f758b
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-05-21 17:16:02 +0300

    Create  24. Countries and Currency Euro.sql

---
+++ CSharp-Databases-Basics/CRUD/24. Countries and Currency Euro.sql
@@ -0,0 +1,15 @@
+USE [Geography]
+
+GO
+
+SELECT * FROM [Countries]
+
+SELECT [CountryName],
+       [CountryCode],
+	   CASE [CurrencyCode]
+	   WHEN 'EUR' THEN 'Euro'
+	   ELSE 'Not Euro'
+	   END
+	   AS [Currency]
+     FROM [Countries]
+	 ORDER BY [CountryName]
