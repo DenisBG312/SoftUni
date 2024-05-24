9d19f259f7e27e00e1f87c7d67047fc6c6ba412e
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-05-24 12:12:49 +0300

    Create 09. *Peaks in Rila.sql

---
+++ CSharp-Databases-Basics/Table Relations/09. *Peaks in Rila.sql
@@ -0,0 +1,14 @@
+SELECT * 
+  FROM [Mountains]
+  WHERE [MountainRange] = 'Rila'
+
+   SELECT [m].MountainRange,
+          [p].PeakName,
+		  [p].[Elevation]
+     FROM [Peaks]
+	   AS [p]
+LEFT JOIN [Mountains]
+	   AS [m]
+	   ON [p].[MountainId] = [m].Id
+	WHERE [m].[MountainRange] = 'Rila'
+	ORDER BY [p].[Elevation] DESC
