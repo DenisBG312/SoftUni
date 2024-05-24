e5a29fd4763533733634191b876cb9c47ccfbe6d
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-05-24 12:00:36 +0300

    Create 06. University Database.sql

---
+++ CSharp-Databases-Basics/Table Relations/06. University Database.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE [University]
+
+GO
+
+USE [University]
+GO
+
+CREATE TABLE [Majors]
+(
+	[MajorID] INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [Subjects]
+(
+	[SubjectID] INT PRIMARY KEY IDENTITY,
+	[SubjectName] VARCHAR(150) NOT NULL
+)
+
+CREATE TABLE [Students]
+(
+	[StudentID] INT PRIMARY KEY IDENTITY,
+	[StudentNumber] VARCHAR(100) NOT NULL,
+	[StudentName] VARCHAR(50) NOT NULL,
+	[MajorID] INT FOREIGN KEY REFERENCES [Majors](MajorID)
+)
+
+CREATE TABLE [Payments]
+(
+	[PaymentID] INT PRIMARY KEY IDENTITY,
+	[PaymentDate] DATETIME2 NOT NULL,
+	[PaymentAmount] DECIMAL(8,2),
+	[StudentID] INT FOREIGN KEY REFERENCES [Students](StudentID) NOT NULL
+)
+
+CREATE TABLE [Agenda]
+(
+	[StudentID] INT FOREIGN KEY REFERENCES [Students](StudentID) NOT NULL,
+	[SubjectID] INT FOREIGN KEY REFERENCES [Subjects](SubjectID) NOT NULL,
+	PRIMARY KEY ([StudentID], [SubjectID])
+)
