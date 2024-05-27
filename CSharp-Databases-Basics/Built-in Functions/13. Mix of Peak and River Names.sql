7ba8fcd53b87853bc6479be9ec1f29eb4c1e62d1
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-05-27 20:22:39 +0300

    Create 13. Mix of Peak and River Names.sql

---
+++ CSharp-Databases-Basics/Built-in Functions/13. Mix of Peak and River Names.sql
@@ -0,0 +1,13 @@
+USE [Geography]
+GO;
+
+SELECT [p].[PeakName],
+	   [r].[RiverName],
+	   LOWER(CONCAT(SUBSTRING([p].[PeakName], 1, LEN([p].[PeakName]) - 1), [r].[RiverName]))
+	   AS [Mix]
+  FROM [Peaks]
+    AS [p],
+       [Rivers]
+	AS [r]
+ WHERE RIGHT(LOWER([p].[PeakName]), 1) = LEFT(LOWER([r].[RiverName]), 1) 
+ ORDER BY [Mix]
