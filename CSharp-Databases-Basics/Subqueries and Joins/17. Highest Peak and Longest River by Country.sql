649491db4bf27b51da92b18d2eb99a037f640a6d
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-03 17:19:34 +0300

    Create 17. Highest Peak and Longest River by Country.sql

---
+++ CSharp-Databases-Basics/Subqueries and Joins/17. Highest Peak and Longest River by Country.sql
@@ -0,0 +1,25 @@
+USE [Geography]
+
+SELECT TOP (5)
+  c.CountryName,
+  MAX(p.Elevation) AS HighestPeakElevation,
+  Max(r.[Length]) AS LongestRiverLength
+  FROM Countries
+    AS c
+  LEFT JOIN MountainsCountries
+         AS mc
+		 ON c.CountryCode = mc.CountryCode
+  LEFT JOIN Mountains
+         AS m
+		 ON mc.MountainId = m.Id
+  LEFT JOIN Peaks
+         AS p
+		 ON m.Id = p.MountainId
+  LEFT JOIN CountriesRivers
+         AS cr
+		 ON c.CountryCode = cr.CountryCode
+  LEFT JOIN Rivers
+         AS r
+		 ON cr.RiverId = r.Id
+GROUP BY c.CountryName
+ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC, c.CountryName
