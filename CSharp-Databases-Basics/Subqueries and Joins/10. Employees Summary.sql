10629171cd9d77c5e80be07836b6b48677886aec
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-03 16:35:50 +0300

    Create 10. Employees Summary.sql

---
+++ CSharp-Databases-Basics/Subqueries and Joins/10. Employees Summary.sql
@@ -0,0 +1,19 @@
+USE SoftUni
+
+SELECT TOP (50)
+     e.EmployeeID,
+	 CONCAT_WS(' ', e.FirstName, e.LastName)
+	        AS EmployeeName,
+	 CONCAT_WS(' ', em.FirstName, em.LastName)
+	        AS ManagerName,
+	  d.[Name]
+	  AS DepartmentName
+    FROM Employees
+      AS e
+    JOIN Employees
+      AS em
+	  ON e.ManagerID = em.EmployeeID
+    JOIN Departments
+      AS d
+	  ON e.DepartmentID = d.DepartmentID
+ORDER BY e.EmployeeID
