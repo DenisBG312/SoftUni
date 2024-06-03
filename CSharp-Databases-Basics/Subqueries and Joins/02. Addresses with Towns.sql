412d331b0b3c34052def3bd508c21ed49f92f516
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-03 15:58:30 +0300

    Create 02. Addresses with Towns.sql

---
+++ CSharp-Databases-Basics/Subqueries and Joins/02. Addresses with Towns.sql
@@ -0,0 +1,16 @@
+USE SoftUni
+
+SELECT TOP (50) 
+       e.FirstName,
+       e.LastName,
+	   t.[Name],
+	   a.AddressText
+  FROM Employees
+    AS e
+  JOIN Addresses
+    AS a
+	ON e.AddressID = a.AddressID
+  JOIN Towns
+    AS t
+    ON a.TownID = t.TownID
+	ORDER BY FirstName ASC, LastName ASC
