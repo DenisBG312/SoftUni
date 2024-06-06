5c8845c7dee9fd55b706ffed92e462523b108588
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-06 16:21:20 +0300

    Create 06. Deposits Sum for Ollivander Family.sql

---
+++ MS-SQL/Indices and Data Aggregation/06. Deposits Sum for Ollivander Family.sql
@@ -0,0 +1,9 @@
+USE Gringotts
+GO
+
+  SELECT DepositGroup,
+         SUM(DepositAmount)
+		 AS TotalSum
+    FROM WizzardDeposits
+   WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY DepositGroup 
