37c7f1b005c17c5a3207350edd5de3b7722e700b
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-08 18:02:36 +0300

    Create 01. Employees with Salary Above 35000.sql

---
+++ MS-SQL/Functions and Stored Procedures/01. Employees with Salary Above 35000.sql
@@ -0,0 +1,11 @@
+USE SoftUni
+GO
+
+CREATE PROCEDURE usp_GetEmployeesSalaryAbove35000
+              AS
+		   BEGIN
+				   SELECT FirstName,
+				          LastName
+				     FROM Employees
+					WHERE Salary > 35000 
+			 END
