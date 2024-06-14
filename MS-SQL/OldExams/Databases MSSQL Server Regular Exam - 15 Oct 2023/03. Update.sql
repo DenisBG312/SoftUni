62179fa4ad01a399653555cbbabf3e686f5772ad
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-14 23:33:45 +0300

    Create 03. Update.sql

---
+++ MS-SQL/OldExams/Databases MSSQL Server Regular Exam - 15 Oct 2023/03. Update.sql
@@ -0,0 +1,7 @@
+UPDATE Bookings
+SET DepartureDate = DATEADD(DAY, 1, DepartureDate)
+WHERE ArrivalDate >= '2023-12-01' AND ArrivalDate <= '2023-12-31'
+
+UPDATE Tourists
+SET Email = NULL
+WHERE [Name] LIKE '%MA%';
