46f74d0b41e7f60d0b670ce235d03172190360c9
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-15 12:13:07 +0300

    Create 10. Hotels Revenue.sql

---
+++ MS-SQL/OldExams/Databases MSSQL Server Regular Exam - 15 Oct 2023/10. Hotels Revenue.sql
@@ -0,0 +1,9 @@
+SELECT 
+	h.[Name],
+	SUM(r.Price * DATEDIFF(DAY, b.ArrivalDate, b.DepartureDate)) AS HotelRevenue
+  FROM Bookings
+    AS b
+  JOIN Hotels AS h ON b.HotelId = h.Id
+  JOIN Rooms AS r ON b.RoomId = r.Id
+  GROUP BY h.[Name]
+  ORDER BY HotelRevenue DESC
