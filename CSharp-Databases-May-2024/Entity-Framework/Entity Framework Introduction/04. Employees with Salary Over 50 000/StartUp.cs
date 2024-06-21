3edf019654798dcd052ef784e6bff8560c155ebb
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-22 00:13:47 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Framework Introduction/04. Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using SoftUni.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50000)
+                .ToList()
+                .OrderBy(e => e.FirstName);
+
+            var sb = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
