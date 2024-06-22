9dfb857bffb0d78a4a0362090e5604be3a2611c5
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-22 17:15:55 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Framework Introduction/14. Delete Project by Id/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using SoftUni.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(DeleteProjectById(context));
+        }
+
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            var project = context.Projects.Find(2);
+            context.Projects.Remove(project);
+
+            var employeeProject = context.EmployeesProjects
+                .Where(e => e.ProjectId == 2)
+                .ToList();
+
+            foreach (var ep in employeeProject)
+            {
+                context.EmployeesProjects.Remove(ep);
+            }
+
+            var projects = context.Projects
+                .Take(10)
+                .Select(p => new
+                {
+                    p.Name
+                });
+
+            context.SaveChanges();
+
+            var sb = new StringBuilder();
+
+            foreach (var p in projects)
+            {
+                sb.AppendLine(p.Name);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
