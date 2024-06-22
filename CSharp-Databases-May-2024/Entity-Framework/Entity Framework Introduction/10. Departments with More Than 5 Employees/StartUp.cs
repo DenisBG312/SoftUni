13051576562dfaa42eb12d4e77d1dec3e9531e9b
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-22 11:47:11 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Framework Introduction/10. Departments with More Than 5 Employees/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using SoftUni.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetDepartmentsWithMoreThan5Employees(context));
+        }
+
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            var departments = context.Departments
+                .Select(e => new
+                {
+                    e.Name,
+                    e.Manager.FirstName,
+                    e.Manager.LastName,
+                    EmployeeCount = e.Employees.Count,
+                    EmployeeInfo = e.Employees
+                        .Select(e => new
+                        {
+                            e.FirstName,
+                            e.LastName,
+                            e.JobTitle
+                        })
+                })
+                .Where(d => d.EmployeeCount > 5)
+                .OrderBy(e => e.EmployeeCount)
+                .ThenBy(d => d.Name);
+
+            var sb = new StringBuilder();
+
+            foreach (var d in departments)
+            {
+                sb.AppendLine($"{d.Name} - {d.FirstName} {d.LastName}");
+                foreach (var e in d.EmployeeInfo
+                             .OrderBy(e => e.FirstName)
+                             .ThenBy(e => e.LastName))
+                {
+                    sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
