a2a6c2eb7f1518bc53599e4ba820e455d9cd0cbc
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-22 11:37:02 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Framework Introduction/09. Employee 147/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using SoftUni.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployee147(context));
+        }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            var searchedEmployee = context.Employees
+                .Select(e => new
+                {
+                    e.EmployeeId,
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    ProjectNames = e.EmployeesProjects
+                        .Select(ep => new
+                        {
+                            ep.Project.Name
+                        })
+                })
+                .Where(e => e.EmployeeId == 147);
+
+            var sb = new StringBuilder();
+            foreach (var e in searchedEmployee)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+                foreach (var p in e.ProjectNames.OrderBy(p => p.Name))
+                {
+                    sb.AppendLine(p.Name);
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
