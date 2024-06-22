32df4c7d66e346e8899b8081aa0223d49e674ea3
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-22 10:05:18 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Framework Introduction/05. Employees from Research and Development/Models/EmployeeProject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUni.Models
+{
+    public partial class EmployeeProject
+    {
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+    }
+}
---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Framework Introduction/05. Employees from Research and Development/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using SoftUni.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    DepartmentName = e.Department.Name,
+                    e.Salary
+                })
+                .Where(e => e.DepartmentName == "Research and Development")
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName);
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.DepartmentName}" +
+                              $" - ${employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+    }
+}
