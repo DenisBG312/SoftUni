0da307e1b8122bca1ba1b04c3eed87a17b2b7e48
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-22 17:37:52 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Framework Introduction/15. Remove Town/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using SoftUni.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(RemoveTown(context));
+        }
+
+        public static string RemoveTown(SoftUniContext context)
+        {
+            var townsToDelete = context.Towns
+                .Where(t => t.Name == "Seattle")
+                .ToList();
+
+            var addressesToDelete = context.Addresses
+                .Where(a => a.Town.Name == "Seattle")
+                .ToList();
+
+            var employeesAddressesToSetNull = context.Employees
+                .Where(e => e.Address.Town.Name == "Seattle")
+                .ToList();
+
+            foreach (var townToDelete in townsToDelete)
+            {
+                context.Towns.Remove(townToDelete);
+            }
+
+            int addressesRemoved = 0;
+
+            foreach (var address in addressesToDelete)
+            {
+                context.Addresses.Remove(address);
+                addressesRemoved++;
+            }
+
+            foreach (var e in employeesAddressesToSetNull)
+            {
+                e.AddressId = null;
+            }
+
+            context.SaveChanges();
+
+            return $"{addressesRemoved} addresses in Seattle were deleted";
+        }
+    }
+}
