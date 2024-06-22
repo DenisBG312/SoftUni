10a598254086d85ea9a0fc6d1cb2e24d2701e9d7
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-22 10:32:04 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Framework Introduction/06. Adding a New Address and Updating Employee/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using SoftUni.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+
+            Console.WriteLine(AddNewAddressToEmployee(context));
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            var newAddress = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            context.Addresses.Add(newAddress);
+            context.SaveChanges();
+
+            var employee = context.Employees
+                .First(e => e.LastName == "Nakov");
+
+            employee.Address = newAddress;
+            context.SaveChanges();
+
+
+            var adresses = context.Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .Select(e => e.Address.AddressText)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var adress in adresses)
+            {
+                sb.AppendLine(adress);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+    }
+}
