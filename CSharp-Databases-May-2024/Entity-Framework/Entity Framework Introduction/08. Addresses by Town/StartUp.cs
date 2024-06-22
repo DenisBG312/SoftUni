91dae2d0aa6a10e670e8098408657691c54760d2
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-22 11:22:37 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Framework Introduction/08. Addresses by Town/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using SoftUni.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetAddressesByTown(context));
+        }
+
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            var addresses = context.Addresses
+                .Select(e => new
+                {
+                    e.AddressText,
+                    TownName = e.Town.Name,
+                    CountOfEmployees = e.Employees.Count
+                })
+                .OrderByDescending(c => c.CountOfEmployees)
+                .ThenBy(t => t.TownName)
+                .ThenBy(a => a.AddressText);
+
+            var sb = new StringBuilder();
+
+            foreach (var a in addresses)
+            {
+                sb.AppendLine($"{a.AddressText}, {a.TownName} - {a.CountOfEmployees} employees");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+    }
+}
