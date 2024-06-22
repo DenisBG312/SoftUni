4eb0441b23e5f60078f0e9bc020538825931bf3f
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-22 13:06:16 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Framework Introduction/12. Increase Salaries/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using SoftUni.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(IncreaseSalaries(context));
+        }
+
+        public static string IncreaseSalaries(SoftUniContext context)
+        {
+            List<string> departmentsCheck = new List<string>();
+            departmentsCheck.Add("Engineering");
+            departmentsCheck.Add("Tool Design");
+            departmentsCheck.Add("Marketing");
+            departmentsCheck.Add("Information Services");
+
+            var employees = context.Employees
+                .Where(e => departmentsCheck.Contains(e.Department.Name))
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var e in employees
+                         .OrderBy(e => e.FirstName)
+                         .ThenBy(e => e.LastName))
+            {
+                var currentSalary = e.Salary;
+                var increasedSalary = currentSalary * 1.12m;
+                e.Salary = increasedSalary;
+                context.Employees.Update(e);
+
+                sb.AppendLine($"{e.FirstName} {e.LastName} (${e.Salary:f2})");
+            }
+
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
