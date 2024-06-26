2eee857fc21f9edcd83f9ad3f70a5c7f6d1d7786
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-26 23:31:32 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Relations/Data/Models/Resource.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.AccessControl;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Resource
+    {
+        [Key]
+        public int ResourceId { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public ResourceType ResourceType { get; set; }
+
+
+        public int CourseId { get; set; }
+
+        [ForeignKey(nameof(CourseId))]
+        public virtual Course Course { get; set; }
+
+
+    }
+
+    public enum ResourceType
+    {
+        Video,
+        Presentation,
+        Document,
+        Other
+    }
+}
---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Relations/Data/StudentSystemContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        private const string ConnectionString =
+            "Server=.;Database=StudentSystem;Integrated Security=True";
+
+        public StudentSystemContext()
+        {
+            
+        }
+
+        public StudentSystemContext(DbContextOptions<StudentSystemContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+        public DbSet<StudentCourse> StudentsCourses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+            modelBuilder.Entity<Student>()
+                .Property(s => s.PhoneNumber)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Resource>()
+                .Property(r => r.Url)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Homework>()
+                .Property(h => h.Content)
+                .IsUnicode(false);
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionString);
+        //}
+    }
+}
