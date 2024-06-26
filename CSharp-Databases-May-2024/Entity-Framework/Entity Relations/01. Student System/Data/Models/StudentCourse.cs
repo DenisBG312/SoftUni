5ec71172fe402516838ef63a2ac2833a776f861a
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-26 23:32:34 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Entity Relations/01. Student System/Data/Models/StudentCourse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class StudentCourse
+    {
+        public int StudentId { get; set; }
+
+        [ForeignKey(nameof(StudentId))]
+        public virtual Student Student { get; set; }
+
+        public int CourseId { get; set; }
+
+        [ForeignKey(nameof(CourseId))]
+        public virtual Course Course { get; set; }
+    }
+}
