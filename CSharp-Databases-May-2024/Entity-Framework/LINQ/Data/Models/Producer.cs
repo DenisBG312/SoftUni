71da18789e13ce1e4b77d2fb44203ad09249461a
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-06-28 22:24:40 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/LINQ/Data/Models/Producer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(ValidationConstants.ProducerNameMaxLength)]
+        public string Name { get; set; } = null!;
+        public string? Pseudonym { get; set; }
+        public string? PhoneNumber { get; set; }
+        public virtual ICollection<Album> Albums { get; set; } = new List<Album>();
+    }
+}
---
+++ CSharp-Databases-May-2024/Entity-Framework/LINQ/Data/Models/Song.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicHub.Data.Models.Enums;
+
+namespace MusicHub.Data.Models
+{
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(ValidationConstants.SongNameMaxLength)]
+        public string Name { get; set; } = null!;
+        public TimeSpan Duration { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Genre Genre { get; set; }
+        public int? AlbumId { get; set; }
+        [ForeignKey(nameof(AlbumId))]
+        public virtual Album? Album { get; set; }
+        public int WriterId { get; set; }
+        [ForeignKey(nameof(WriterId))] 
+        public virtual Writer Writer { get; set; } = null!;
+        public decimal Price { get; set; }
+        public virtual ICollection<SongPerformer> SongPerformers { get; set; } = new List<SongPerformer>();
+    }
+}
