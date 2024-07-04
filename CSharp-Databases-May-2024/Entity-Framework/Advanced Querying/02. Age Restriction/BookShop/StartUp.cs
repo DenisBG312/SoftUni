547f4961b6650541070c039cbcbdf0749847a9eb
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-07-04 20:03:36 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Advanced Querying/02. Age Restriction/BookShop/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+
+            Console.WriteLine(GetBooksByAgeRestriction(context, "miNor"));
+            //DbInitializer.ResetDatabase(db);
+        }
+
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var books = context.Books
+                .ToArray()
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .Select(b => new
+                {
+                    b.Title
+                })
+                .OrderBy(t => t.Title);
+
+            var sb = new StringBuilder();
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.Title);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
+
+
