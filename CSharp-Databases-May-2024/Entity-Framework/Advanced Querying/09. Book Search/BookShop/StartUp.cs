f50e15953e1e82715f2df9563ff0ddb123b79937
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-07-07 00:51:56 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Advanced Querying/09. Book Search/BookShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+using BookShop.Models.Enums;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+
+            Console.WriteLine(GetBookTitlesContaining(context, "WOR"));
+        }
+
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            var books = context.Books
+                .Where(b => b.Title.ToLower().Contains(input.ToLower()))
+                .Select(b => new
+                {
+                    b.Title
+                })
+                .OrderBy(b => b.Title);
+
+            var sb = new StringBuilder();
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.Title);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
+
+
