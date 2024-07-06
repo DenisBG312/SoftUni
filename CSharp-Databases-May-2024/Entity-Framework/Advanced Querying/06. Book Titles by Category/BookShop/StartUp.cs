c1f2f59e380776ffd0c52294288e5c6ae16b6c40
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-07-07 00:48:13 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Advanced Querying/06. Book Titles by Category/BookShop/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+using BookShop.Models.Enums;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+
+            Console.WriteLine(GetBooksByCategory(context, "horror mystery drama"));
+
+            //DbInitializer.ResetDatabase(db);
+        }
+
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            var categoriesList = input
+                .ToLower()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var books = context.BooksCategories
+                .Where(c => categoriesList.Contains(c.Category.Name.ToLower()))
+                .Select(b => new
+                {
+                    b.Book.Title
+                })
+                .OrderBy(b => b.Title);
+
+            var sb = new StringBuilder();
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.Title);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
+
+
