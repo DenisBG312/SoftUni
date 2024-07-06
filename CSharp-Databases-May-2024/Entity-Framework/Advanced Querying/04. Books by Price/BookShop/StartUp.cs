78c6d96d41232913e78297f879eab56cdfb1e31d
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-07-07 00:45:56 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Advanced Querying/04. Books by Price/BookShop/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using BookShop.Models.Enums;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            Console.WriteLine(GetBooksByPrice(context));
+            //DbInitializer.ResetDatabase(db);
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(t => t.Price > 40)
+                .Select(t => new
+                {
+                    t.Title,
+                    t.Price
+                })
+                .OrderByDescending(t => t.Price);
+
+            var sb = new StringBuilder();
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} - ${book.Price:f2}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
+
+
