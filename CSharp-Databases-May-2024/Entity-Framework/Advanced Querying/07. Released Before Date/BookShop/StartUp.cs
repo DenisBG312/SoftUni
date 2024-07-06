02ae1aa131927d7c72c6046d566d067438f64eb2
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-07-07 00:49:18 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Advanced Querying/07. Released Before Date/BookShop/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+using BookShop.Models.Enums;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+
+            Console.WriteLine(GetBooksReleasedBefore(context, "30-12-1989"));
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            var dateTime = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            var books = context.Books
+                .Where(b => b.ReleaseDate < dateTime)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.EditionType,
+                    b.Price,
+                    b.ReleaseDate
+                })
+                .OrderByDescending(b => b.ReleaseDate);
+
+            var sb = new StringBuilder();
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} - {book.EditionType} - ${book.Price:f2}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+
+
+    }
+}
+
+
