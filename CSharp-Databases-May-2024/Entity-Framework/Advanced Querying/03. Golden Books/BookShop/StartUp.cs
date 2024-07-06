258fe7a125e3869b53e844f4c16cdc3ecc4e03ff
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-07-07 00:44:34 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Advanced Querying/03. Golden Books/BookShop/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using BookShop.Models.Enums;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+
+            Console.WriteLine(GetGoldenBooks(context));
+            //DbInitializer.ResetDatabase(db);
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var goldenBooks = context.Books
+                .Where(t => t.Copies < 5000 && t.EditionType == EditionType.Gold)
+                .Select(t => new
+                {
+                    t.Title,
+                    t.BookId,
+                    t.Copies
+                })
+                .OrderBy(t => t.BookId);
+
+            var sb = new StringBuilder();
+
+            foreach (var book in goldenBooks)
+            {
+                sb.AppendLine(book.Title);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
+
+
