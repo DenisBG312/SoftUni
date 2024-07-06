9720328de5395336cfb8ff1f1e824c640a48f061
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-07-07 00:50:18 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Advanced Querying/08. Author Search/BookShop/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+using BookShop.Models.Enums;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+
+            Console.WriteLine(GetAuthorNamesEndingIn(context, "e"));
+        }
+
+
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        {
+            var authors = context.Authors
+                .Where(a => a.FirstName.EndsWith(input))
+                .Select(a => new
+                {
+                   a.FirstName,
+                   a.LastName
+                })
+                .OrderBy(a => a.FirstName)
+                .ThenBy(a => a.LastName);
+
+            var sb = new StringBuilder();
+
+            foreach (var author in authors)
+            {
+                sb.AppendLine($"{author.FirstName} {author.LastName}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
+
+
