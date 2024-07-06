2971d9794aae653b43d2a72aef58f5924e993bf5
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-07-07 00:47:18 +0300

    Add files via upload

---
+++ CSharp-Databases-May-2024/Entity-Framework/Advanced Querying/05. Not Released In/BookShop/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+using BookShop.Models.Enums;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            Console.WriteLine(GetBooksNotReleasedIn(context, 1998));
+            //DbInitializer.ResetDatabase(db);
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate.HasValue 
+                            && b.ReleaseDate.Value.Year != year)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.ReleaseDate,
+                    b.BookId
+                })
+                .OrderBy(b => b.BookId);
+
+            var sb = new StringBuilder();
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.Title);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
+
+
