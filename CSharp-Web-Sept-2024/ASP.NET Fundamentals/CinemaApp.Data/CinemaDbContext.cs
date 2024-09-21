ec348382f0d690a4a394f31a6f7dfcb05a766c90
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2024-09-22 01:51:09 +0300

    Add files via upload

---
+++ CSharp-Web-Sept-2024/ASP.NET Fundamentals/CinemaApp.Data/CinemaDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using CinemaApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaApp.Data
+{
+    public class CinemaDbContext : DbContext
+    {
+        public CinemaDbContext()
+        {
+
+        }
+
+        public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ CSharp-Web-Sept-2024/ASP.NET Fundamentals/CinemaApp.Data/Migrations/20240921210742_Initial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CinemaApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Director", "Duration", "Genre", "ReleaseDate", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("11390c9c-031c-4044-a8e2-bcb525b19586"), "The Lord of the Rings is an epic[1] high fantasy novel[a] by the English author and scholar J. R. R. Tolkien. Set in Middle-earth, the story began as a sequel to Tolkien's 1937 children's book The Hobbit, but eventually developed into a much larger work. Written in stages between 1937 and 1949.", "Peter Jackson", 178, "Fantasy", new DateTime(2001, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Lord of the Rings" },
+                    { new Guid("7e0014cd-d326-476c-8c9d-95484d7b0304"), "Harry Potter and the Goblet of Fire is a 2005 fantasy film directed by Mike Newell from a screenplay by Steve Kloves. It is based on the 2000 novel Harry Potter and the Goblet of Fire by J. K. Rowling.", "Mike Newel", 157, "Fantasy", new DateTime(2011, 11, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Harry Potter and the Goblet of Fire" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
