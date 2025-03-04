76fed8bdb8e5853dd432c6c78806aad266886b59
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2025-03-04 21:59:36 +0200

    Delete JS-FrontEnd-Jan-2025/JS Front-End/3. List Of Names.js

--- JS-FrontEnd-Jan-2025/JS Front-End/3. List Of Names.js
+++
@@ -1,7 +0,0 @@
-function solve(strArr){
-    let sortedArr = strArr.sort((a, b) => a.localeCompare(b));
-    
-    for (let i = 0; i < sortedArr.length; i++) {
-        console.log(`${i+1}.${sortedArr[i]}`);
-    }
-}
