88b6f9ab8e4dcd7a9c3ce1f5ce796652bb6aa3f1
Denis Tsranski <113765690+DenisBG312@users.noreply.github.com>
2025-03-04 21:46:56 +0200

    Update 1. Array Rotation.js

--- JS-FrontEnd-Jan-2025/JS Front-End/JS Arrays and Strings - Exercise/1. Array Rotation.js
+++ JS-FrontEnd-Jan-2025/JS Front-End/JS Arrays and Strings - Exercise/1. Array Rotation.js
@@ -1,1 +1,15 @@
+function solve(arr, rotations) {
+    let result = [];
+    let startIndex = rotations % arr.length;
 
+    for (let i = startIndex; i < arr.length; i++) {
+        result.push(arr[i]);
+    }
+
+    for (let i = 0; i < startIndex; i++) {
+        result.push(arr[i]);
+    }
+
+
+    console.log(result.join(' '));
+}
